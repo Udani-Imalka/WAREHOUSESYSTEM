a5a36d299042ad6ce67e5457c1da46bb5f853704
Udani Imalka <uimalka.96@gmail.com>
2021-12-17 23:13:04 +0530

    add file structure

---
+++ Frontend/warehouse_frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="row">
+      <Header/>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
